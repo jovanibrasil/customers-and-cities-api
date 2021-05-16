de85d802a2198c19fca3c22e3c4d51f8c471a8f1
Jovani Brasil <jovani.brasil@compasso.com.br>
2021-05-16 16:38:36 -0300

    Adjust cors configuration

--- src/infra/http/express.js
+++ src/infra/http/express.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const { successHandler, errorHandler } = require('../loggers/monganConfig')
 const load = require('express-load');
 const app = express();
 const errorHandlerMiddleware = require('../middlewares/ErrorValidationMiddleware');
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(successHandler);
